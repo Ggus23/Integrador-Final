bccccba2fa993d4c35b75034a5ad9f0b53e311ac
Ggus23 <pacaragustin@gmail.com>
2026-08-18 23:24:45 -0400

    arreglando variables

--- menta-link/frontend/lib/api.ts
+++ menta-link/frontend/lib/api.ts
@@ -1,8 +1,12 @@
 const getAPIUrl = () => {
+  if (process.env.NEXT_PUBLIC_API_BASE_URL) {
+    return process.env.NEXT_PUBLIC_API_BASE_URL;
+  }
   if (typeof window !== 'undefined') {
     return `${window.location.protocol}//${window.location.hostname}:8000`;
   }
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
+
+  return 'http://localhost:8000';
 };
 
 const API_URL = getAPIUrl();
