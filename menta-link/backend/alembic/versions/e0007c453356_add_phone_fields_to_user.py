de2cafc7714b20a8272d67735fa3cc27346a8d10
Ggus23 <pacaragustin@gmail.com>
2026-09-18 22:15:29 -0400

    feat: add phone fields to user model-2

--- menta-link/backend/alembic/versions/e0007c453356_add_phone_fields_to_user.py
+++ menta-link/backend/alembic/versions/e0007c453356_add_phone_fields_to_user.py
@@ -7,9 +7,9 @@ Create Date: 2026-09-18 22:08:35.162451
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'e0007c453356'
